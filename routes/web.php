642e594496ae53653e974cf7371938fa692e9cac
luckyboy002 <151297077+luckyboy002@users.noreply.github.com>
2025-10-24 08:41:12 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\MataKuliahController;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,16 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/profile/{nama}/{NPM}/{class}',[ProfileController::class,'profile']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
-Route::post('/user', [UserController::class, 'store'])->name('user.store');
-Route::get('/matakuliah', [MataKuliahController::class, 'index'])->name('mata-kuliah.index');
-Route::get('/matakuliah/create', [MataKuliahController::class, 'create'])->name('mata-kuliah.create');
-Route::post('/matakuliah/store', [MataKuliahController::class, 'store'])->name('mata-kuliah.store');
-Route::get('/mata-kuliah/{id}/edit', [MataKuliahController::class, 'edit'])->name('mata-kuliah.edit');
-Route::put('/mata-kuliah/{id}', [MataKuliahController::class, 'update'])->name('mata-kuliah.update');
-Route::delete('/mata-kuliah/{id}', [MataKuliahController::class, 'destroy'])->name('mata-kuliah.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
+require __DIR__.'/auth.php';
